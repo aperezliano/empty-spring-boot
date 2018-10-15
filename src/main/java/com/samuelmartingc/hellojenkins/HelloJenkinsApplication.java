1ba38dde76e710176afe323ad7b5a66de6cf0a39
Samuel Martin <samumartin@ebay.com>
2018-10-15 17:59:16 +0200

    mvn project

---
+++ src/main/java/com/samuelmartingc/hellojenkins/HelloJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.samuelmartingc.hellojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJenkinsApplication.class, args);
+	}
+}
